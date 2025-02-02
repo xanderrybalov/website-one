c4521eb08dc017f846a2eaa85752c0a9b3bb8a4a
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-02-02 17:43:06 +0200

    fix: text-overlay fix

--- src/lib/TextOverlay.svelte
+++ src/lib/TextOverlay.svelte
@@ -1,35 +1,16 @@
-<script>
-    let { season, title } = $props(); 
-  </script>
+<script lang="ts">
+  let { season, title } = $props();
+</script>
+
+<div 
+  class="absolute text-white text-left leading-[0.75] top-[29rem] left-[3.8rem] 
+  font-[Garamond,serif] tracking-[-0.02em]">
   
-  <div class="text-overlay">
-    <span class="season">{season}</span>
-    <br />
-    <span class="title">{title}</span>
-  </div>
-  
-  <style>
-    .text-overlay {
-      position: absolute;
-      top: 30rem;
-      left: 3.8rem;
-      color: white;
-      text-align: left;
-      line-height: 0.75;
-    }
-  
-    .season {
-      font-size: 11rem;
-      font-weight: 400;
-      letter-spacing: -0.02em; /* Регулирует расстояние между буквами */
-      font-family: "Garamond", serif; /* Можно попробовать Didot или другой схожий */
-    }
-  
-    .title {
-      font-size: 11rem;
-      font-weight: 400;
-      letter-spacing: -0.02em;
-      font-family: "Garamond", serif;
-    }
-  </style>
-  
+  <span class="block font-light xl:text-[11rem] lg:text-[6rem] ">
+    {season}
+  </span>
+
+  <span class="block font-light xl:text-[11rem] lg:text-[6rem] ">
+    {title}
+  </span>
+</div>
